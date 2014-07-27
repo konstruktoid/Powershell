ffb4180143901bb352ed1e202e5772fc53c31fcb
konstruktoid <konstruktoid@users.noreply.github.com>
2014-07-27 14:26:46 +0200

    New line

--- getOfflineVMs.ps1
+++ getOfflineVMs.ps1
@@ -2,6 +2,7 @@ if (!(Get-PSSnapin -Name VMware.VimAutomation.Core -ErrorAction SilentlyContinue
 {
             Add-PSSnapin VMware.VimAutomation.Core
 }
+
 Connect-VIServer -Server SERVER -User USER -Password PASSWORD
 
 Get-VM  | where {$_.powerstate -eq "PoweredOff"} | Select Name, VMHost, PowerState, Version, Description | Sort-Object Name
