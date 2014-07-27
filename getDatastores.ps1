c6f1c77756c48593d88831f8e68c09325bbe74e5
konstruktoid <konstruktoid@users.noreply.github.com>
2014-07-27 14:08:26 +0200

    Messy chars

--- getDatastores.ps1
+++ getDatastores.ps1
@@ -8,4 +8,4 @@ Connect-VIServer -Server SERVER -User USER -Password PASSWORD
 Get-Datacenter | Get-Datastore |  where {$_.Name -match "SAN"} | Select Name, Datacenter, 
     @{N="Capacity Gb";E={[Math]::Round($_.CapacityGB)}}, 
     @{N="FreeSpace Gb";E={[Math]::Round($_.FreespaceGB)}},     @{N="FreeSpace %";E={[Math]::Round(($_.FreespaceGB / $_.CapacityGB) * 100)}},
-    @{N=”Provisioned %”;E={[Math]::Round(((($_.CapacityGB – $_.FreespaceGB +($_.extensiondata.summary.uncommitted/1GB))/$_.CapacityGB))*100)}} | Sort-Object Name 
+    @{N="Provisioned %";E={[Math]::Round(((($_.CapacityGB - $_.FreespaceGB +($_.extensiondata.summary.uncommitted/1GB))/$_.CapacityGB))*100)}} | Sort-Object Name 
