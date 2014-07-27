6021f20c0b640b34b2f42abf98e436f848fa2a27
konstruktoid <konstruktoid@users.noreply.github.com>
2014-07-27 14:17:37 +0200

    Messy chars

--- getvSphereHW.ps1
+++ getvSphereHW.ps1
@@ -6,6 +6,6 @@ if (!(Get-PSSnapin -Name VMware.VimAutomation.Core -ErrorAction SilentlyContinue
 Connect-VIServer -Server SERVER -User USER -Password PASSWORD
 
 Get-VMHost | Get-View | Select Name, Parent, 
-@{N=“Type“;E={$_.Hardware.SystemInfo.Vendor+ “ “ + $_.Hardware.SystemInfo.Model}},
-@{N=“CPU“;E={“PROC:“ + $_.Hardware.CpuInfo.NumCpuPackages + “ CORES:“ + $_.Hardware.CpuInfo.NumCpuCores + “ MHZ: “ + [math]::round($_.Hardware.CpuInfo.Hz / 1000000, 0)}},
-@{N=“MEM“;E={“” + [math]::round($_.Hardware.MemorySize / 1GB, 0) + “ GB“}} | Sort-Object Name
+@{N="Type";E={$_.Hardware.SystemInfo.Vendor+ " " + $_.Hardware.SystemInfo.Model}},
+@{N="CPU";E={"PROC:" + $_.Hardware.CpuInfo.NumCpuPackages + " CORES:" + $_.Hardware.CpuInfo.NumCpuCores + " MHZ: " + [math]::round($_.Hardware.CpuInfo.Hz / 1000000, 0)}},
+@{N="MEM";E={"" + [math]::round($_.Hardware.MemorySize / 1GB, 0) + " GB"}} | Sort-Object Name
