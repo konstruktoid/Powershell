b499254525add37793d4758117246d0d3a69a980
konstruktoid <konstruktoid@users.noreply.github.com>
2014-07-27 14:00:41 +0200

    getDatastores
    Get datastore capacity

---
+++ getDatastores.ps1
@@ -0,0 +1,11 @@
+if (!(Get-PSSnapin -Name VMware.VimAutomation.Core -ErrorAction SilentlyContinue))
+{
+            Add-PSSnapin VMware.VimAutomation.Core
+}
+
+Connect-VIServer -Server SERVER -User USER -Password PASSWORD
+
+Get-Datacenter | Get-Datastore |  where {$_.Name -match "SAN"} | Select Name, Datacenter, 
+    @{N="Capacity Gb";E={[Math]::Round($_.CapacityGB)}}, 
+    @{N="FreeSpace Gb";E={[Math]::Round($_.FreespaceGB)}},     @{N="FreeSpace %";E={[Math]::Round(($_.FreespaceGB / $_.CapacityGB) * 100)}},
+    @{N=”Provisioned %”;E={[Math]::Round(((($_.CapacityGB – $_.FreespaceGB +($_.extensiondata.summary.uncommitted/1GB))/$_.CapacityGB))*100)}} | Sort-Object Name 
